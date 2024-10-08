b306cbfbbbe6b251f193aba54d5d326e4b8a9320
Anthony Vladimir Gotay <39626593+VladlmirG@users.noreply.github.com>
2024-10-08 13:23:36 +0200

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -133,9 +133,37 @@ const LoginPage = () => {
   };
 
   return (
-    <div className="h-[calc(100vh-50px)] px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 flex items-center justify-center ">
-      <form className="flex flex-col gap-8 shadow-[0_3px_10px_rgb(0,0,0,0.2)] p-8 rounded-lg" onSubmit={handleSubmit}>
-        <h1 className="text-2xl font-semibold">{formTitle}</h1>
+    <>
+
+      {/* SVG WAVE SHAPE */}
+      <div className="custom-shape-divider-bottom hidden md:block">
+          <svg
+            className="absolute bottom-0 w-full"
+            data-name="Layer 1"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 1200 120"
+            preserveAspectRatio="none"
+          >
+            <path
+              d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z"
+              opacity=".25"
+              className="shape-fill"
+            ></path>
+            <path
+              d="M0,0V15.81C13,36.92,27.64,56.86,47.69,72.05,99.41,111.27,165,111,224.58,91.58c31.15-10.15,60.09-26.07,89.67-39.8,40.92-19,84.73-46,130.83-49.67,36.26-2.85,70.9,9.42,98.6,31.56,31.77,25.39,62.32,62,103.63,73,40.44,10.79,81.35-6.69,119.13-24.28s75.16-39,116.92-43.05c59.73-5.85,113.28,22.88,168.9,38.84,30.2,8.66,59,6.17,87.09-7.5,22.43-10.89,48-26.93,60.65-49.24V0Z"
+              opacity=".5"
+              className="shape-fill"
+            ></path>
+            <path
+              d="M0,0V5.63C149.93,59,314.09,71.32,475.83,42.57c43-7.64,84.23-20.12,127.61-26.46,59-8.63,112.48,12.24,165.56,35.4C827.93,77.22,886,95.24,951.2,90c86.53-7,172.46-45.71,248.8-84.81V0Z"
+              className="shape-fill"
+            ></path>
+          </svg>
+      </div>
+      
+    <div className="h-[calc(80vh-50px)] px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 flex items-center justify-center loginbg">
+      <form className="flex flex-col gap-8 shadow-[0_3px_10px_rgb(0,0,0,0.2)] p-8 rounded-lg bg-white loginform" onSubmit={handleSubmit}>
+        <h1 className="text-5xl six_caps-font text-bluey tracking-wider">{formTitle}</h1>
         {mode === MODE.REGISTER ? (
           <div className="flex flex-col gap-2">
             <label className="text-sm text-gray-700">Username</label>
@@ -192,7 +220,7 @@ const LoginPage = () => {
           </div>
         )}
         <button
-          className="bg-button text-white p-2 rounded-md disabled:bg-black disabled:cursor-not-allowed hover:bg-hovr transition duration-500"
+          className="bg-bluey text-pinky p-2 rounded-md disabled:bg-black disabled:cursor-not-allowed hover:bg-pinky hover:text-bluey transition duration-500"
           disabled={isLoading}
         >
           {isLoading ? "Loading..." : buttonTitle}
@@ -225,6 +253,7 @@ const LoginPage = () => {
         {message && <div className="text-green-600 text-sm">{message}</div>}
       </form>
     </div>
+      </>
   );
 };
 
